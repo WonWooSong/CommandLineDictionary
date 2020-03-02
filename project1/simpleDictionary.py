7b9b020334e20577ac12a7df90179075803f7659
wonwoo.song <songw@unbc.ca>
2020-03-02 02:01:56 -0800

    added to catch non-existing word and throw warning.

--- project1/simpleDictionary.py
+++ project1/simpleDictionary.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def dictionary(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "This word does not exist in dictionary. Please check your spelling."
 
 user_input = input("Enter a word: ")
 output = dictionary(user_input)
