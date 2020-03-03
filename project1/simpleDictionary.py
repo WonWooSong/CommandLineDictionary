946cb488c51460e073465702b1884926932b4107
wonwoo.song <songw@unbc.ca>
2020-03-03 04:29:08 -0800

    update

--- project1/simpleDictionary.py
+++ project1/simpleDictionary.py
@@ -1,11 +1,15 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def dictionary(word):
     word = word.lower()
+    close_words = get_close_matches(word, data.keys(), cutoff=0.75)
     if word in data:
         return data[word]
+    elif len(close_words) > 0:
+        return "Did you mean \"%s\" instead?" % close_words[0]
     else:
         return "This word does not exist in dictionary. Please check your spelling."
 
