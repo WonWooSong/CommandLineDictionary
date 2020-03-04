e54196071ff72b9fed405b648c0eba3ac2352085
wonwoo.song <songw@unbc.ca>
2020-03-03 19:02:02 -0800

    updated simple dictionary to be more user friendly.

--- project1/simpleDictionary.py
+++ project1/simpleDictionary.py
@@ -8,11 +8,23 @@ def dictionary(word):
     close_words = get_close_matches(word, data.keys(), cutoff=0.75)
     if word in data:
         return data[word]
-    elif len(close_words) > 0:
-        return "Did you mean \"%s\" instead?" % close_words[0]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yesOrNo = input("Did you mean \"%s\" instead? Enter Y/y if yes, or N/n if no: " % get_close_matches(word, data.keys())[0])
+
+        if yesOrNo == "Y" or "y" or "Yes" or "yes":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yesOrNo == "N" or "n" or "No" or "no": 
+            return "This word does not exist in dictionary. Please check your spelling."
+        else:
+            return "Please input valid word."
     else:
         return "This word does not exist in dictionary. Please check your spelling."
 
 user_input = input("Enter a word: ")
 output = dictionary(user_input)
-print(output)
+
+if type(output) == list:
+    for item in output:
+        print("- " + item)
+else:
+    print("- " +output)
