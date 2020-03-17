c116d37acab7ad5df1985932ecaee57af9f15cb1
wonwoo.song <songw@unbc.ca>
2020-03-17 00:18:06 -0700

    added commandline feature using argparse

--- project1/simpleDictionary.py
+++ project1/simpleDictionary.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 from difflib import get_close_matches
 
 data = json.load(open("data.json"))
@@ -11,17 +12,21 @@ def dictionary(word):
     elif len(get_close_matches(word, data.keys())) > 0:
         yesOrNo = input("Did you mean \"%s\" instead? Enter Y/y if yes, or N/n if no: " % get_close_matches(word, data.keys())[0])
 
-        if yesOrNo == "Y" or "y" or "Yes" or "yes":
+        if yesOrNo == "Y" or yesOrNo == "y" or yesOrNo == "Yes" or yesOrNo == "yes":
             return data[get_close_matches(word, data.keys())[0]]
-        elif yesOrNo == "N" or "n" or "No" or "no": 
+        elif yesOrNo == "N" or yesOrNo == "n" or yesOrNo == "No" or yesOrNo == "no": 
             return "This word does not exist in dictionary. Please check your spelling."
         else:
             return "Please input valid word."
     else:
         return "This word does not exist in dictionary. Please check your spelling."
 
-user_input = input("Enter a word: ")
-output = dictionary(user_input)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-w", "--word", type=str, help="Finding word's dictionary meaning.")
+args = parser.parse_args()    
+
+output = dictionary(args.word)
 
 if type(output) == list:
     for item in output:
